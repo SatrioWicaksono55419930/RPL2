e1bc5303a39228062da96be71f4a883455145b5b
SatrioWicaksono55419930 <satriowicak37@gmail.com>
2022-11-23 17:01:32 +0700

    pertemuan 5

--- src/com/toko/buku/service/TokoBukuService.java
+++ src/com/toko/buku/service/TokoBukuService.java
@@ -1,4 +1,9 @@
-package com.toko.buku.dao;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.toko.buku.service;
 
 import com.toko.buku.model.TokoBuku;
 import java.util.List;
@@ -7,7 +12,7 @@ import java.util.List;
  *
  * @author ASUS
  */
-public interface TokoBukuDao {
+public interface TokoBukuService {
     public void save(TokoBuku tokobuku);
     public void update(TokoBuku tokobuku);
     public void delete(TokoBuku tokobuku);
---
+++ src/com/toko/buku/service/TokoBukuServiceImpl.java
@@ -0,0 +1,44 @@
+package com.toko.buku.service;
+
+import com.toko.buku.dao.TokoBukuDao;
+import com.toko.buku.model.TokoBuku;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author ASUS
+ */
+@Service("TokoBukuService")
+@Transactional(readOnly = true)
+public class TokoBukuServiceImpl implements TokoBukuService {
+    @Autowired
+    private TokoBukuDao tokoBukuDao;
+
+    @Override
+    public void save(TokoBuku tokobuku) {
+        tokoBukuDao.save(tokobuku);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(TokoBuku tokobuku) {
+        tokoBukuDao.update(tokobuku);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(TokoBuku tokobuku) {
+        tokoBukuDao.delete(tokobuku);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<TokoBuku> getList() {
+        return tokoBukuDao.getList();
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
